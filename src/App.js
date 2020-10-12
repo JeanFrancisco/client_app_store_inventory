84963b219f8df265f04e195aeaf5b2b461b17af3
Jean Frankxcezco <j_juanfrancisco_jmz@yahoo.com.mx>
2020-10-12 07:31:48 -0500

    Added IconTabs from Material-Ui to navigate between future components. Added new TabPanel component to use as a linked container(s) for each icon in the AppBar. Used the useState hook from React Hooks to manage Tab selection. Used the makeStyles & useStyles Hook to customize the individual styles for each App element used: one Paper from Material-Ui as appGlobalContainer and other AppBar from Material-Ui as appNavigationBar.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AppBar, Paper, Tabs, Tab } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { AttachMoney, Timeline, ListAlt, FlightLand, Settings } from '@material-ui/icons';
+import TabPanel from './components/misc/TabPanel';
+
+const useStyles = makeStyles( theme => ({
+    appGlobalContainer: {
+      paddingBottom: 50
+    },
+    appNavigationBar: {
+      top: 'auto',
+      bottom: 0
+    }
+  })
+);
 
 function App() {
+  const classes = useStyles();
+
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleTabSelection = (e, new_selected_tab) => {
+    setSelectedTab(new_selected_tab);
+  }
+
   return (
     <div className="App">
+      <Paper square className={ classes.appGlobalContainer } elevation={ 2 }>
+        <TabPanel value={ selectedTab } index={ 0 }>
+
+        </TabPanel>
+        <TabPanel value={ selectedTab } index={ 1 }>
+
+        </TabPanel>
+      </Paper>
+      <AppBar position="fixed" color="default" className={ classes.appNavigationBar }>
+        <Tabs
+          value={ selectedTab }
+          onChange={ handleTabSelection }
+          variant="fullWidth"
+          indicatorColor="secondary"
+          textColor="secondary"
+        >
+          <Tab icon={ <AttachMoney/> } label="Vender"/>
+          <Tab icon={ <Timeline/> } label="Ventas"/>
+          <Tab icon={ <ListAlt/> } label="Nuevo Pedido"/>
+          <Tab icon={ <FlightLand/> } label="Recibidos"/>
+          <Tab icon={ <Settings/> } label="Configuracion"/>
+        </Tabs>
+      </AppBar>
     </div>
   );
 }
