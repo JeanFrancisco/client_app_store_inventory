4547bf78534bf63dc8e7ad7407f26e68892a1bb5
Jean Frankxcezco <j_juanfrancisco_jmz@yahoo.com.mx>
2020-10-19 14:12:02 -0500

    Added the development|debug tools: Redux DevTools Extension. IMPORTANT You need to add this extension to the web browser, the block of code in this commit only adds the call to the function if it exists or it is available. Please visit GitHub: zalmoxisus|redux-devtools-extension for more information

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import appReducers from './reducers';
 
-const store = createStore(appReducers);
+const store = createStore(
+    appReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
